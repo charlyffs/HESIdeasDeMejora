4c6130fc201ea41ce7d78d844abd713b29bf689e
Carlos M <carlosedm@protonmail.com>
2021-11-08 14:09:29 -0700

    Axios demo

--- src/views/Prototyping.vue
+++ src/views/Prototyping.vue
@@ -1,9 +1,21 @@
 <template>
-  <div></div>
+  <div>
+    <p>{{ text }}</p>
+  </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  data() {
+    axios
+      .get("https://swapi.dev/api/people/1")
+      .then((response) => (this.text = response.data.name));
+    return {
+      text: String,
+    };
+  },
+};
 </script>
 
 <style></style>
