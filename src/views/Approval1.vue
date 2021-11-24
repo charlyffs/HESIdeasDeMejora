60d722478c664aadc1cc211eabcf0e75fdd55604
mireyacabrera <mireya.cabrera@cetys.edu.mx>
2021-11-23 23:52:28 -0800

    modificaciones en approval 1, 2 y assignment

--- src/views/Approval1.vue
+++ src/views/Approval1.vue
@@ -1,3 +1,13 @@
+<!-- comentarios generales
+    - al cargar la pantalla se ocupan:
+            - los datos de la idea propuesta
+    - el botón cerrar abre un cuadro de confirmación (no estoy segura de a dónde debe regresar)
+    - el botón aprobación abre un cuadro de diálogo donde habrán las opciones Aprobar y Rechazar
+        - el botón Aceptar dentro del cuadro de diálogo de Aprobación hará un método POST porque debe guardar si la idea se aprobó o rechazó 
+        y mandar a la 2da aprobación en caso de que sea aprobada
+    
+ -->
+
 <template>
   <v-container>
     <v-row>
@@ -6,106 +16,170 @@
         <h3>Primera aprobación</h3>
       </v-col>
     </v-row>
-    <v-row justify="left">
+    <v-row>
       <div class = "inicio">
          <p>INFORMACIÓN GENERAL <span id = "icon">PENDIENTE DE REVISIÓN (HES)</span></p>
       </div>
     </v-row>
     <v-row>
-        <v-dialog
-      v-model="dialog"
-      width="500">
-        <template v-slot:activator="{ on, attrs }">
         <p>
-        <v-btn x-small plain>
-            Cerrar
-        </v-btn>
+        <v-dialog v-model="dialogCerrar" width="500">
+            <template v-slot:activator="{ on, attrs }">
+                <v-btn x-small plain v-bind="attrs" v-on="on">
+                    Cerrar
+                </v-btn>
+            </template>
+            <v-card>
+                <v-card-title class="text-h5 grey lighten-2">
+                    Cerrar propuesta
+                </v-card-title>
+
+                <div id="propuestaFactible">
+                <h3 class="ma-4">¿Desea cerrar esta propuesta?</h3>
+                <v-card-actions>
+                    <v-row>
+                        <v-col>
+                            <v-btn block color="blue" class="ma-2 pa-2 white--text">
+                                CANCELAR
+                            </v-btn>
+                        </v-col>
+                        <v-col>
+                            <v-btn block color="blue" class="ma-2 pa-2 white--text" to="/">
+                                ACEPTAR
+                            </v-btn>
+                        </v-col>
+                    </v-row>
+                </v-card-actions>
+                </div>
+            </v-card>
+
+        </v-dialog>
         |
-        <v-btn x-small plain>
-            Editar gral.
-        </v-btn>
-        |
-        <v-btn x-small plain>
-            Eliminar
-        </v-btn>
-        |
-        <v-btn x-small plain v-bind="attrs" v-on="on">
-            Aprobación
-        </v-btn>
         </p>
-        </template>
-
-        <v-card>
-        <v-card-title class="text-h5 grey lighten-2">
-          Aprobación
-        </v-card-title>
-
-        <div id="aprobarRechazar">
-        <v-card-actions>
-            <v-radio-group v-model="primeraAprobacion" row>
-                <v-radio label="Aprobar" value="aprobar"></v-radio>
-                <v-radio label="Rechazar" value="rechazar"></v-radio>
-            </v-radio-group>
-        </v-card-actions>
-
-        <v-card-actions>
-            <v-col>
-                <v-btn class="ma-2" block>
-                    Cancelar
+        <v-dialog v-model="dialogAprobacion" width="500">
+            <template v-slot:activator="{ on, attrs }">
+                <v-btn x-small plain v-bind="attrs" v-on="on">
+                    Aprobación
                 </v-btn>
-            </v-col>
+            </template>
 
-            <v-col>
-                <v-btn class="ma-2" block @click:>
-                    Aceptar
-                </v-btn>
-            <p>radiobutton: {{ primeraAprobacion }}</p>
-            </v-col>
-        </v-card-actions>
-        </div>
-        </v-card>
+            <v-card>
+                <v-card-title class="text-h5 grey lighten-2">
+                    Aprobación
+                </v-card-title>
+
+                <div id="aprobarRechazar">
+                <v-card-actions>
+                    <v-radio-group v-model="primeraAprobacion" row class="ma-2">
+                        <v-radio label="Aprobar" value="aprobar"></v-radio>
+                        <v-radio label="Rechazar" value="rechazar"></v-radio>
+                    </v-radio-group>
+                </v-card-actions>
+                    
+                    <div v-if="primeraAprobacion == 'aprobar'">
+                        <v-card-actions>
+                            <v-row>
+                                <v-col>
+                                    <v-select v-model="tipoMejora" :items="tipoMejoraItems" label="Tipo de mejora" class="ma-2"></v-select>
+                                </v-col>
+                            </v-row>
+                        </v-card-actions>
+                    </div>
+                    <div v-else>
+                        <v-card-actions>
+                            <v-row>
+                                <v-select v-model="rechazoJustificacion" :items="rechazoJustificacionItems" label="Justificación" class="ma-2"></v-select>
+                            </v-row>
+                        </v-card-actions>
+
+                        <v-card-actions>
+                            <v-row>
+                                <v-text-field v-model="rechazoComentarios" label="Comentarios" value="" solo class="ma-2"></v-text-field>
+                            </v-row>
+                        </v-card-actions>
+                    </div>
+
+
+                <v-card-actions>
+                        <v-row>
+                            <v-col>
+                                <v-btn block color="blue" class="ma-2 white--text" @click="dialogAprobacion = false">
+                                    CANCELAR
+                                </v-btn>
+                            </v-col>
+                            <v-col>
+                                <v-btn block color="blue" class="ma-2 white--text" @click="aceptar()">
+                                    ACEPTAR
+                                </v-btn>
+                            </v-col>
+                        </v-row>
+                    </v-card-actions>
+                </div>
+            </v-card>
         </v-dialog>
       </v-row>
     
     <div class="infogeneral">
         <v-row>
             <v-col>
-                <p align="center"># Propuesta</p>
-                <p align="center">21-001</p>
+                <v-text name="hola"># Propuesta</v-text>
             </v-col>
             <v-col>
-                <p align="center">Propuesta por</p>
-                <p align="center">JESÚS CARRILLO</p> 
+                <v-text>Propuesta por</v-text>
             </v-col>
             <v-col>
-                <p align="center">UAP/DEPTO</p>
-                <p align="center">HES</p>
+                <v-text>UAP/DEPTO</v-text>
             </v-col>
             <v-col>
-                <p align="center">Área de quien propone</p>
-                <p align="center">HES</p>
+                <v-text>Área de quien propone</v-text>
             </v-col>
             <v-col>
-                <p align="center">Fecha</p>
-                <p align="center">04/08/2021</p>
+                <v-text>Fecha</v-text>
             </v-col>
         </v-row>    
         <v-row>
             <v-col>
-                <p align="center">Gnte. de quien</p>
-                <p align="center">JESÚS CARRILLO</p>
+                <v-text v-model="numPropuesta">21-001</v-text>
             </v-col>
             <v-col>
-                <p align="center">Área de la Mejora</p>
-                <p align="center">Almacén</p>
+                <v-text v-model="nombreEmisor">JESÚS CARRILLO</v-text> 
             </v-col>
             <v-col>
-                <p align="center">Supervisor del área de mejora</p>
-                <p align="center">AARON MANRIQUEZ</p>
+                <v-text v-model="nombreDepto">HES</v-text>
             </v-col>
             <v-col>
-                <p align="center">Tipo de Mejora</p>
-                <p align="center">Movimientos</p>
+                <v-text v-model="areaPropone">HES</v-text>
+            </v-col>
+            <v-col>
+                <v-text v-model="fecha">04/08/2021</v-text>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col>
+                <v-text>Gnte. de quien</v-text>
+            </v-col>
+            <v-col>
+                <v-text>Área de la Mejora</v-text>
+            </v-col>
+            <v-col>
+                <v-text>Supervisor del área de mejora</v-text>
+            </v-col>
+            <v-col>
+                <v-text>Tipo de Mejora</v-text>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col>
+                <v-text v-model="nombreGnte">JESÚS CARRILLO</v-text>
+            </v-col>
+            <v-col>
+                <v-text v-model="areaMejora">Almacén</v-text>
+            </v-col>
+            <v-col>
+                <v-text v-model="nombreSupervisor">AARON MANRIQUEZ</v-text>
+            </v-col>
+            <v-col>
+                <v-text v-model="tipoMejora">Movimientos</v-text>
             </v-col>
         </v-row>
         </div>
@@ -116,35 +190,35 @@
         <v-row>
             <v-col>
                 <p>Título de la propuesta</p>
-                <v-text-field value="" solo disabled></v-text-field>
+                <v-text-field v-model="tituloPropuesta" value="Hola" solo readonly></v-text-field>
             </v-col>
         </v-row>
         <v-row>
             <v-col>
                 <p>Oportunidad de mejora</p>
-                <v-text-field value="" solo disabled></v-text-field>
+                <v-text-field v-model="opMejora" value="" solo readonly></v-text-field>
             </v-col>
         </v-row>
         <v-row>
             <v-col>
                 <p>(Antes)</p>
-                <v-img max-height="100%" max-width="100%" src="" aspect-ratio="1.7"></v-img>
+                <v-img v-model="imgAntes" max-height="100%" max-width="100%" src="../assets/prueba.png" aspect-ratio="1.7"></v-img>
             </v-col>
         </v-row>
         <v-row>
             <v-col>
                 <p>Propuesta de mejora</p>
-                <v-textarea name="input-7-1" value="" disabled solo auto-grow hint="Hint text"></v-textarea>
+                <v-textarea v-model="propuestaMejora" value="" readonly solo auto-grow></v-textarea>
             </v-col>
         </v-row>
         </div>
         <template>
             <v-row>
-                <v-col class="ma-2">
-                    <v-btn block color="blue" class="white--text">Guardar</v-btn>
+                <v-col>
+                    <v-btn block color="blue" class="ma-2 white--text">Guardar</v-btn>
                 </v-col>
                 <v-col>
-                    <v-btn block color="blue" class="white--text">Finalizar</v-btn>
+                    <v-btn block color="blue" class="ma-2 white--text">Finalizar</v-btn>
                 </v-col>
             </v-row>
         </template>
@@ -153,14 +227,44 @@
 
 <script>
     export default {
-    data () {
-      return {
-        primeraAprobacion: null,
-      }
+    data: () => ({
+        dialogAprobacion: false,
+        dialogCerrar: false,
+        tipoMejoraItems: ['Movimientos', 'Inventario', 'Proceso / Organización', 'Sobreproducción', 'Tiempo', 'Calidad: Defectos/Retrabajos', 'Esperas', 'Costos / Ahorros', 'Medio ambiente / Energía'],
+        rechazoJustificacionItems: ['Existe un proyecto relacionado con la mejora', 'No es viable por inversión', 'No es idea, es corrección', 'No es idea, es necesidad'],
+        primeraAprobacion: "aprobar",
+        propuestaMejora: "",
+        opMejora: "",
+        imgAntes: "",
+        nombreDepto: "",
+        tituloPropuesta: "",
+        tipoMejora: "",
+        nombreSupervisor: "",
+        areaMejora: "",
+        nombreGnte: "",
+        fecha: "",
+        areaPropone: "",
+        nombreEmisor: "",
+        numPropuesta: "",
+        rechazoComentarios: "",
+        rechazoJustificacion: null,
+        aprobar: null,
+        rechazar: null,
+    }),
+    methods:
+    {
+        aceptar()
+        {
+            // método post
+            // se necesita: 
+        },
+        cancelar()
+        {
+            this.dialogAprobacion = false;
+        }
     }
   }
-
-
+  
 </script>
 
 <style>
